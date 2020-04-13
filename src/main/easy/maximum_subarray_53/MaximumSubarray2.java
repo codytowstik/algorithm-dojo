1f3b7fce0b34789085b4ad96767f934f226eb4c8
codytowstik <cody@towstik.com>
2020-04-12 23:49:09 -0400

    * maximum subarray optimal solution

--- src/main/easy/maximum_subarray_53/MaximumSubarray2.java
+++ src/main/easy/maximum_subarray_53/MaximumSubarray2.java
@@ -1,27 +1,34 @@
 package main.easy.maximum_subarray_53;
 
-import java.util.Arrays;
-
 public class MaximumSubarray2 extends MaximumSubarray
 {
 	/**
-	 * SingleNumber.
+	 * Kadane's Algorithm
 	 *
-	 * Stream implementation.
+	 * Runtime: O(n), 0ms, 100%
+	 * Space: O(1), 39.2mb, 9.39%
 	 *
-	 * 4ms, 39.6%
-	 * 43.3mb, 5.19%
+	 * Like version 1, but set best_sum and current_sum to the first number in the array
 	 *
-	 * @param input
-	 * @return
+	 * @param input the array
+	 * @return the sum of the maximum subarray
 	 */
 	public int maxSubArray(int[] input)
 	{
-		int singleNumber =
-			Arrays
-				.stream(input)
-				.reduce(0, (collector, value) -> collector ^= value);
+		int current_sum = input[0];
+		int best_sum = input[0];
 
-		return singleNumber;
+		for (int i = 1; i < input.length; i++)
+		{
+			int 	currentNumber = input[i];
+
+			// reset current sum to current number if this sequence of numbers is negative (therefore won't add to any sequence)
+			current_sum = Math.max(currentNumber, current_sum + currentNumber);
+
+			best_sum = Math.max(best_sum, current_sum);
+		}
+
+
+		return best_sum;
 	}
 }
